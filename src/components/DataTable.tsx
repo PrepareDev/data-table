40a93d93af9ba2ebe0d7f62f0787c5d7fe1edc05
snusEbjoer <pojiratelsnusa@gmail.com>
2023-09-30 21:26:44 +0300

    Some fixes after reviev Ref: SNU-18

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -2,31 +2,23 @@ import React from "react"
 
 export type DataTableProps = {
     table: Record<string, string | number>[]
-
+    headers: string[]
 }
 
+export const DataTable: React.FC<DataTableProps> = ({ table, headers }) => {
 
-
-export const DataTable: React.FC<DataTableProps> = ({ table }) => {
-
-    const tableHeaders = Object.keys(table[0])
-    const rows = Object.values(table)
-
-    console.log(rows)
     return (
         <div className="">
-            <h1 className="font-bold text-5xl"> Data Table Project </h1>
             <table >
                 <thead>
                     <tr>
-                        {tableHeaders.map(k => <th>{k}</th>)}
+                        {headers.map((k, i) => <th key={i}>{k}</th>)}
                     </tr>
                 </thead>
                 <tbody>
-                    {rows.map(row => <tr>{Object.values(row).map(v => <td>{v}</td>)}</tr>)}
+                    {table.map(row => <tr>{Object.values(row).map((v, i) => <td key={i}>{v}</td>)}</tr>)}
                 </tbody>
             </table>
-            <h2 className="text-4xl"> Implement me </h2>
         </div>
     )
 }

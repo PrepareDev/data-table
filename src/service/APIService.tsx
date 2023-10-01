2ab9bebf2bfabd0df5b21ef9e1b76ccb4ff7840d
snusEbjoer <pojiratelsnusa@gmail.com>
2023-10-01 20:54:32 +0300

    added APIService class with pagination methods and fetchdata

--- src/service/APIService.tsx
+++ src/service/APIService.tsx
@@ -2,9 +2,7 @@ export class APIService {
 
     private readonly BASE_URL = "http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}"
     private page = 1
-    /**
-     * fetchData
-     */
+
     async fetchData() {
         const res = await fetch(this.BASE_URL)
         return res.json()
